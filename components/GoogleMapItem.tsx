7981cbab22ce29d9ecfad2ddbb230f0b01b76b14
Hadrien Jaubert <hadrien.jaubert99@gmail.com>
2022-12-02 10:24:15 +0100

    project done

---
+++ components/GoogleMapItem.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+const GoogleMapItem = ({ item }) => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <div style={{marginTop: "5px", marginBottom: "5px"}}>
+            <Button variant="primary" onClick={handleShow}>
+                {item.name}
+            </Button>
+
+            <Modal show={show} onHide={handleClose} dialogClassName="my-modal" style={{marginTop: "50px", alignSelf: "center"}}>
+                <Modal.Body>
+                    {item.image}
+                </Modal.Body>
+                <Modal.Footer>
+                    <a href={item.link} download>Télécharger</a>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+export default GoogleMapItem;
